9e51bcfc12ab58dc4b5a3ff8ab07741f998aefca
dtacao <caoanhdao18041996@gmail.com>
2019-04-10 15:53:11 +0700

    Add /api/ for product type

--- src/main/java/com/view/batdongsanfrontend/service/BaseService.java
+++ src/main/java/com/view/batdongsanfrontend/service/BaseService.java
@@ -1,5 +1,5 @@
 package com.view.batdongsanfrontend.service;
 
 public class BaseService {
-    protected String ROOT_URL = "http://localhost:8080/";
+    protected String ROOT_URL = "http://localhost:8080/api/";
 }
