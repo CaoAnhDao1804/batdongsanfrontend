e7dd31336c8ff77ddc8483672904eaa47b896486
dtacao <caoanhdao18041996@gmail.com>
2019-06-01 14:43:04 +0700

    Change base service

--- src/main/java/com/view/batdongsanfrontend/service/BaseService.java
+++ src/main/java/com/view/batdongsanfrontend/service/BaseService.java
@@ -5,8 +5,8 @@ import com.view.batdongsanfrontend.model.Post;
 import org.springframework.web.multipart.MultipartFile;
 
 public class BaseService {
-//    protected String ROOT_URL = "https://batdongsandanangapi.herokuapp.com/api/";
-    protected String ROOT_URL = "http://localhost:8080/api/";
+    protected String ROOT_URL = "https://batdongsandanangapi.herokuapp.com/api/";
+//    protected String ROOT_URL = "http://localhost:8080/api/";
 
 
 }
